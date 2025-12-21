5237d734c9a47b8d99c69f7c08d6ea756123fae8
t0k4r <mi03to08@gmail.com>
2025-12-21 21:11:27 +0100

    add test for /pretty endpoint

--- json-tools/src/test/java/com/example/json_tools/JsonPrettyTest.java
+++ json-tools/src/test/java/com/example/json_tools/JsonPrettyTest.java
@@ -1,12 +1,47 @@
 package com.example.json_tools;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.http.MediaType;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@WebMvcTest(JsonPretty.class)
 class JsonPrettyTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void pretty() {
+    void returnsPretty() throws Exception {
+        String requestJson = "{\"hello\":\"world\",\"languages\":[\"c\",\"c++\"],\"abc\":{\"yyz\":\"lax\"}}";
+        String responseJson = "{\n" +
+                "  \"hello\" : \"world\",\n" +
+                "  \"languages\" : [ \"c\", \"c++\" ],\n" +
+                "  \"abc\" : {\n" +
+                "    \"yyz\" : \"lax\"\n" +
+                "  }\n" +
+                "}";
+
+        mockMvc.perform(post("/pretty")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                        .andExpect(status().isOk())
+                        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(content().string(responseJson));
+    }
+
+
+    @Test
+    void rejectsInvalid() throws Exception {
+        String requestJson = "{\"hello\":\"world\",\"yyz\"}";
+
+        mockMvc.perform(post("/pretty")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isBadRequest());
     }
 }
